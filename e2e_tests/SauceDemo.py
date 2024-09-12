1cf2cf39cbd653c47fb59af26a28a05d21b0c574
TimurRakhimov <lollipopnote8@gmail.com>
2024-09-12 23:56:51 +0300

    Add test_adding_and_removing_good_to_cart, test_purchase

--- e2e_tests/SauceDemo.py
+++ e2e_tests/SauceDemo.py
@@ -1,14 +1,22 @@
 import random
 import unittest
 
+from dataclasses import dataclass
+from faker import Faker
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
 class SauceDemo(unittest.TestCase):
+    @dataclass
+    class CartItem:
+        name: str = ""
+        desc: str = ""
+        price: str = ""
 
     def setUp(self):
         self.driver = webdriver.Chrome()
+        self.faker = Faker()
         self.root_url = "https://www.saucedemo.com/"
 
     def test_standard_authorization(self):
@@ -27,7 +35,7 @@ class SauceDemo(unittest.TestCase):
         self.assertEqual(cookies[0]['name'], 'session-username')
         self.assertNotEqual(len(cookies[0]['value']), 0)
 
-    def test_adding_one_good_to_cart(self):
+    def test_adding_one_good_to_cart(self, cart_item: CartItem = None):
         self.test_standard_authorization()
 
         inventory_items_count = len(self.driver.find_elements(By.CLASS_NAME, "btn_inventory"))
@@ -61,8 +69,62 @@ class SauceDemo(unittest.TestCase):
         self.assertEqual(inventory_item_desc, cart_item_desc)
         self.assertEqual(inventory_item_price, cart_item_price)
 
+        if cart_item is not None:
+            cart_item.name = cart_item_name
+            cart_item.desc = cart_item_desc
+            cart_item.price = cart_item_price.split("$")[1]
+
+    def test_adding_and_removing_good_to_cart(self):
+        self.test_adding_one_good_to_cart()
+
+        for cart_item in self.driver.find_elements(By.CLASS_NAME, "cart_item"):
+            cart_item.find_element(By.CLASS_NAME, "cart_button").click()
+
+        self.assertEqual(len(self.driver.find_elements(By.CLASS_NAME, "cart_item")), 0)
+        self.driver.refresh()
+        self.assertEqual(len(self.driver.find_elements(By.CLASS_NAME, "cart_item")), 0)
+
     def test_purchase(self):
-        pass
+        cart_item = self.CartItem()
+        self.test_adding_one_good_to_cart(cart_item)
+
+        checkout_btn = self.driver.find_element(By.ID, "checkout")
+        checkout_btn.click()
+
+        first_name = self.driver.find_element(By.ID, "first-name")
+        first_name.send_keys(self.faker.first_name())
+        last_name = self.driver.find_element(By.ID, "last-name")
+        last_name.send_keys(self.faker.last_name())
+        postal_code = self.driver.find_element(By.ID, "postal-code")
+        postal_code.send_keys(self.faker.postcode())
+
+        continue_btn = self.driver.find_element(By.ID, "continue")
+        continue_btn.click()
+
+        checkout_items = self.driver.find_elements(By.CLASS_NAME, "cart_item")
+        self.assertEqual(len(checkout_items), 1)
+
+        cart_item_name = checkout_items[0].find_element(By.CLASS_NAME, "inventory_item_name").text
+        cart_item_desc = checkout_items[0].find_element(By.CLASS_NAME, "inventory_item_desc").text
+        cart_item_price = checkout_items[0].find_element(By.CLASS_NAME, "inventory_item_price").text.split("$")[1]
+
+        self.assertEqual(cart_item.name, cart_item_name)
+        self.assertEqual(cart_item.desc, cart_item_desc)
+        self.assertEqual(cart_item.price, cart_item_price)
+
+        summary_subtotal = self.driver.find_element(By.CLASS_NAME, "summary_subtotal_label").text.split("$")[1]
+        self.assertEqual(summary_subtotal, cart_item.price)
+
+        summary_tax = self.driver.find_element(By.CLASS_NAME, "summary_tax_label").text.split("$")[1]
+        summary_total = self.driver.find_element(By.CLASS_NAME, "summary_total_label").text.split("$")[1]
+
+        self.assertEqual(round(float(summary_subtotal) + float(summary_tax), 2), round(float(summary_total), 2))
+
+        finish_btn = self.driver.find_element(By.ID, "finish")
+        finish_btn.click()
+
+        complete_headers = self.driver.find_elements(By.CLASS_NAME, "complete-header")
+        self.assertEqual(len(complete_headers), 1)
 
     def tearDown(self):
         self.driver.close()
--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import unittest
 import e2e_tests
 
 if __name__ == '__main__':
-    unittest.main(module=e2e_tests)
+    unittest.main(module=e2e_tests, verbosity=2)
